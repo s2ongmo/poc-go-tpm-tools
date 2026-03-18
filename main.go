4d9c3b107265284bce29845f9731ddebe519622c
seongmo <48545752+s2ongmo@users.noreply.github.com>
2026-03-18 14:43:38 +0900

    Fix main.go encoding

--- main.go
+++ main.go
@@ -5,9 +5,7 @@ import "fmt"
 const Version = "1.0.0"
 
 func main() {
-	fmt.Printf("gotpm-poc version %s
-", Version)
+	fmt.Printf("gotpm-poc version %s\n", Version)
 	fmt.Println("This is a PoC binary simulating google/go-tpm-tools gotpm CLI.")
-	fmt.Println("If this binary has been tampered with, you will see evidence below:")
 	fmt.Println("INTEGRITY_CHECK=CLEAN")
 }
